9bd1b97806c5b6a61f81730e7db1fb36bff3013b
cdmccall91 <49795002+cdmccall91@users.noreply.github.com>
2021-01-28 17:46:58 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,88 +1,96 @@
-// VARIABLES
-
-const newDay = document.getElementById('start-new-day');
-const container = document.getElementById('container');
+// Variables
+const newDayBtn = document.getElementById('new-day-btn');
 const closeBtn = document.getElementById('close');
-const modal = document.getElementById('modal');
 const pushIt = document.getElementById('submit-btn');
-const dateInput = document.getElementById('date');
+const dayGrid = document.querySelector('.day-grid');
+const date = document.getElementById('date');
 
-// FUNCTIONS
+// Functions
 function createNewDay(e) {
   e.preventDefault();
-  
   // Hiding modal before card creation
-  modal.classList.remove('show-modal');
+  modal.classList.remove('show');
 
-  // Creating Day Card
-  const newDay = document.createElement('div');
-  newDay.classList.add('day-card');
-  container.appendChild(newDay);
+  // Creating New Card
+  const newDayCard = document.createElement('div');
+  newDayCard.className = "day-card";
+  dayGrid.appendChild(newDayCard);
 
-  // Puting Date at top of card
-  const date = dateInput.value;
-  const dateTitle = document.createElement('h2');
-  dateTitle.classList.add('date-title');
-  dateTitle.textContent = date;
-  newDay.appendChild(dateTitle);
+  // Create Card Header
+  const cardHeader = document.createElement('div');
+  cardHeader.className= "card-header";
+  newDayCard.appendChild(cardHeader);
 
-  // Creating New Set Button
-  const newSet = document.createElement('button');
-  newSet.classList.add('new-set');
-  newSet.textContent = 'Start New Set';
-  newDay.appendChild(newSet);
+  // Creating The Date
+  const newDate = document.createElement('span');
+  newDate.className = "new-date";
+  newDate.textContent = date.value;
+  cardHeader.appendChild(newDate);
 
-  // Creating Rep List Container
-  const repListContainer = document.createElement('div');
-  repListContainer.classList.add('rep-list-container');
-  newDay.appendChild(repListContainer);
+  // Creating The Total
+  const total = document.createElement('span');
+  total.className = "total";
+  total.textContent = "total: " + 0;
+  cardHeader.appendChild(total);
 
-  // Create Rep Input Container
-  const enterRepContainer = document.createElement('div');
-  repListContainer.appendChild(enterRepContainer);
+  // Create Middle that will store set lists
+  const setGrid = document.createElement('div');
+  setGrid.className = "set-grid";
+  newDayCard.appendChild(setGrid);
 
-  // Creating UL Rep List
-  const repList = document.createElement('ul');
-  repListContainer.insertBefore(repList, enterRepContainer);
+  // Create New Set Container with Rep List and Input/Submit at bottom
+  const newSetListContainer = document.createElement('div');
+  newSetListContainer.className = "set-list-container";
+  setGrid.appendChild(newSetListContainer);
 
-  // Creating Enter Rep Input
-  const enterRep = document.createElement('input');
-  enterRep.type = 'number';
-  enterRep.placeholder = 'Enter Rep';
-  enterRep.classList.add('rep-input');
-  enterRepContainer.appendChild(enterRep);
+  // Create Card Footer
+  const cardFooter = document.createElement('div');
+  cardFooter.className = "card-footer";
+  newDayCard.appendChild(cardFooter);
 
-  // Creating Submit button for Enter Rep
-  const submitRep = document.createElement('button');
-  submitRep.classList.add('submit-rep');
-  submitRep.textContent = 'Nice!';
-  enterRepContainer.appendChild(submitRep);
+  // Create New Set Button
+  const newSetBtn = document.createElement('button');
+  newSetBtn.className = "new-set-btn";
+  newSetBtn.textContent = "Start New Set";
+  cardFooter.appendChild(newSetBtn);
 
-  // Adding Reps to an UL
-  submitRep.addEventListener('click', function addRep() {
-    const rep = enterRep.value;
-    const addRep = document.createElement('li');
-    addRep.textContent = rep;
-    repList.appendChild(addRep);
-    enterRep.value = '';
+  // Start New Set Event
+  newSetBtn.addEventListener('click', function startNewSet() {
+      // Create new rep list
+  const newSetList = document.createElement('ul');
+  newSetList.className = "set-list";
+  newSetListContainer.appendChild(newSetList);
+  // Create input for reps
+  const repInput = document.createElement('input');
+  repInput.className = "rep-input";
+  repInput.type = "number";
+  repInput.placeholder = "Enter Reps";
+  newSetListContainer.appendChild(repInput);
+  // Create button to submit input reps
+  const repSubmitBtn = document.createElement('button');
+  repSubmitBtn.className = "rep-submit-btn";
+  repSubmitBtn.textContent = "Nice!";
+  newSetListContainer.appendChild(repSubmitBtn);
+  // Adding Reps to the list
+  repSubmitBtn.addEventListener('click', function addRep() {
+    const newRep = document.createElement('li')
+    const rep = repInput.value;
+    newRep.textContent = rep;
+    newSetList.appendChild(newRep);
+    repInput.value = '';
   });
-
-
+  })
 }
 
-
-
-// EVENT LISTENERS
-
-// Remove modal outside click
-window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
+// Event Listeners
 // Display modal when click start new day button
-newDay.addEventListener('click', () => modal.classList.add('show-modal'));
-// Hide modal when click on X button
-closeBtn.addEventListener('click', () => modal.classList.remove('show-modal'));
+newDayBtn.addEventListener('click', () => modal.classList.add('show'));
+// Hide modal on outside click
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show') : false);
+// Hidw with X button
+closeBtn.addEventListener('click', () => modal.classList.remove('show'));
 
 // Create New Day
 pushIt.addEventListener('click', createNewDay);
-
 
 
